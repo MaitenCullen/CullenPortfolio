761895a79e00ef8955ae02931ab67ae4ab3adba6
MaitenCullen <cullen.maiten@gmail.com>
2023-08-28 10:38:23 -0300

    navbar error -resolve-

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ export const NavBar = () => {
           <ul className='mobileNavbar'>
               <li id={theme} className='clean navLink'><a  href='#skills' onClick={hideMenu}>{t('navbar.SKILLS')}</a></li>
               <li className='clean navLink'><a  href='#projects' onClick={hideMenu}>{t('navbar.PROJECT')}</a></li>
-              <li className='clean navLink'><a  href='#servicios' onClick={hideMenu}>{t('navbar.CONTACT')}</a></li>
+              <li className='clean navLink'><a  href='#contact' onClick={hideMenu}>{t('navbar.CONTACT')}</a></li>
               <div className='div-icon-mobile'>
                  <i className="icon-nav md-25"  onClick={hideMenu}><ImCross/></i>
               </div>
